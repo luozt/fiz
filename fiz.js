87e9c1f7b69140ff3054e643eff4126db0fd2df7
luozt <luozt@foxmail.com>
2016-05-16 14:36:54 +0800

    修复使用get命令还会调用fis的问题；

--- fiz.js
+++ fiz.js
@@ -23,141 +23,143 @@ cli.launch({
   //捕获命令行
   var command = argv["_"];
 
-  //fiz get项目
-  if("get" == command[0]){
-    //提供的服务
-    var targetHash = {
-      "web": {
-        name: "web-template",
-        url: "https://github.com/luozt/fiz-web-template.git"
-      }
-    };
+  switch(command[0]){
+    // fiz get
+    case "get":
+      //提供的服务
+      var targetHash = {
+        "web": {
+          name: "web-template",
+          url: "https://github.com/luozt/fiz-web-template.git"
+        }
+      };
 
-    //检测是否存在该服务
-    var cmd1 = command[1];
-    var targetObj = targetHash[cmd1];
-    if(!targetObj){
-      console.log("Not such \"" + cmd1 + "\" get-target!");
-      return false;
-    }
-
-    var noDir = !!argv["n"];
-    var folder = targetObj.name;
-    var childProcess = require("child_process");
-    var exec = childProcess.exec;
-
-    console.log("start get [ " + cmd1 + " ]");
-
-    var child = exec("git clone " + targetObj.url, function(err, stdout, stderr){
-      if(err){
-        console.log("git clone err: " + err);
-        return;
+      //检测是否存在该服务
+      var cmd1 = command[1];
+      var targetObj = targetHash[cmd1];
+      if(!targetObj){
+        console.log("Not such \"" + cmd1 + "\" get-target!");
+        return false;
       }
 
-      console.log("get [ " + cmd1 + " ] success");
+      var noDir = !!argv["n"];
+      var folder = targetObj.name;
+      var childProcess = require("child_process");
+      var exec = childProcess.exec;
 
-      var child2 = exec("rm -rf " + folder + "/.git", function(err, stdout){
-        if (err){
-          console.log("rm "+ folder +"/.git error");
+      console.log("start get [ " + cmd1 + " ]");
+
+      var child = exec("git clone " + targetObj.url, function(err, stdout, stderr){
+        if(err){
+          console.log("git clone err: " + err);
           return;
         }
 
-        if (noDir){
-          var child3 = exec("cp -rf " + folder + "/* ./", function(err, stdout){
-            if(err){
-              console.log("cp " + folder + " folder error");
-              return;
-            }
+        console.log("get [ " + cmd1 + " ] success");
 
-            var child4 = exec("rm -rf " + folder, function(err, stdout){
-              if (err){
-                console.log("rm "+ folder +" folder error")
+        var child2 = exec("rm -rf " + folder + "/.git", function(err, stdout){
+          if (err){
+            console.log("rm "+ folder +"/.git error");
+            return;
+          }
+
+          if (noDir){
+            var child3 = exec("cp -rf " + folder + "/* ./", function(err, stdout){
+              if(err){
+                console.log("cp " + folder + " folder error");
                 return;
               }
+
+              var child4 = exec("rm -rf " + folder, function(err, stdout){
+                if (err){
+                  console.log("rm "+ folder +" folder error")
+                  return;
+                }
+              });
             });
-          });
-        }
+          }
+        });
       });
-    });
-  }
+      break;
 
-  // 移动文件夹，不支持文件
-  if("mvDirContent" == command[0]){
-    var childProcess, exec, dest, folder, child, cwd;
+    // 移动文件夹，不支持文件
+    case "mvDirContent":
+      var childProcess, exec, dest, folder, child, cwd;
 
-    cwd = process.cwd();
-    folder = argv["src"];
-    dest = argv["to"];
+      cwd = process.cwd();
+      folder = argv["src"];
+      dest = argv["to"];
 
-    if(!dest || "string" != typeof dest){
-      console.log("dest: "+dest+", operate destination required!");
-      return false;
-    }
+      if(!dest || "string" != typeof dest){
+        console.log("dest: "+dest+", operate destination required!");
+        return false;
+      }
 
-    if(!folder || "string" !== typeof folder){
-      folder = cwd;
-    }
+      if(!folder || "string" !== typeof folder){
+        folder = cwd;
+      }
 
-    childProcess = require("child_process");
-    exec = childProcess.exec;
+      childProcess = require("child_process");
+      exec = childProcess.exec;
 
-    if(!fs.existsSync(folder)){
-      console.log("source: "+folder+", not such a source Foler!");
-      return;
-    }
-
-    if("/" === folder.slice(-1)){
-      folder = folder.slice(0,-1);
-    }
-
-    console.log("source folder: "+folder);
-    console.log("destination folder: "+dest);
-
-    child = exec("mv -f "+folder+"/** "+dest, function(err, stdout){
-      var child2;
-
-      if(err){
-        console.log(err);
-        console.log("mv "+folder+" folder content error when execing: "+"mv -f "+folder+"/** "+dest);
+      if(!fs.existsSync(folder)){
+        console.log("source: "+folder+", not such a source Foler!");
         return;
       }
 
-      child2 = exec("rm -rf "+folder, function(err, stdout){
-        var child3, clearFolder;
+      if("/" === folder.slice(-1)){
+        folder = folder.slice(0,-1);
+      }
+
+      console.log("source folder: "+folder);
+      console.log("destination folder: "+dest);
+
+      child = exec("mv -f "+folder+"/** "+dest, function(err, stdout){
+        var child2;
 
         if(err){
           console.log(err);
-          console.log("rm "+folder+" folder error");
+          console.log("mv "+folder+" folder content error when execing: "+"mv -f "+folder+"/** "+dest);
           return;
         }
 
-        // 检测是否还有要删除的文件夹
-        clearFolder = argv["clear"];
-        if(clearFolder){
-          child3 = exec("rm -rf "+clearFolder, function(err, stdout){
-            if(err){
-              console.log(err);
-              console.log("rm "+clearFolder+" folder error");
-              return;
-            }
-          });
-        }
+        child2 = exec("rm -rf "+folder, function(err, stdout){
+          var child3, clearFolder;
+
+          if(err){
+            console.log(err);
+            console.log("rm "+folder+" folder error");
+            return;
+          }
+
+          // 检测是否还有要删除的文件夹
+          clearFolder = argv["clear"];
+          if(clearFolder){
+            child3 = exec("rm -rf "+clearFolder, function(err, stdout){
+              if(err){
+                console.log(err);
+                console.log("rm "+clearFolder+" folder error");
+                return;
+              }
+            });
+          }
+        });
       });
-    });
-  }
+      break;
 
-  //引用fis
-  else{
-    var fis;
-    if(!env.modulePath){
-      fis = require("./");
-    }else{
-      fis = require(env.modulePath);
-    }
+    // 引用fis
+    default:
+      var fis;
 
-    fis.set("system.localNPMFolder", path.join(env.cwd, "node_modules/fiz"));
-    //fis.set('system.globalNPMFolder', path.dirname(__dirname));
-    fis.cli.run(argv, env);
+      if(!env.modulePath){
+        fis = require("./");
+      }else{
+        fis = require(env.modulePath);
+      }
+
+      fis.set("system.localNPMFolder", path.join(env.cwd, "node_modules/fiz"));
+      // fis.set('system.globalNPMFolder', path.dirname(__dirname));
+      fis.cli.run(argv, env);
   }
 
 });
